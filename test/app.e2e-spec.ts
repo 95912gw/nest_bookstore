dd550dcf3cbcf00bba87bb5ddc592a65b8665d29
95912gw <gw777seong@gmail.com>
2021-02-23 14:51:44 +0900

    Add e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,92 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import {INestApplication, ValidationPipe} from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+        new ValidationPipe({
+          whitelist: true,
+          forbidNonWhitelisted: true,
+          transform: true,
+        })
+    );
     await app.init();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+        .get('/')
+        .expect(200)
+        .expect('Welcome');
+  });
+
+  describe('/books', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+          .get('/books')
+          .expect(200)
+          .expect([])
+    });
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+          .post('/books')
+          .send({
+            "title": "titleBook",
+            "author": "authorBook",
+            "year": 2021,
+            "genres": ["genre1", "genre2"]
+          })
+          .expect(201);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+          .post('/books')
+          .send({
+            "title": "titleBook",
+            "author": "authorBook",
+            "year": 2021,
+            "genres": ["genre1", "genre2"],
+            "others": "something"
+          })
+          .expect(400);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+          .delete('/books')
+          .expect(404)
+    });
+  });
+
+  describe('/books/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+          .get('/books/999')
+          .expect(404);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+          .get('/books/1')
+          .expect(200);
+    });
+    it('PATCH', () => {
+      return request(app.getHttpServer())
+          .patch('/books/1')
+          .send({title: "updateBook"})
+          .expect(200);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+          .delete('/books/1')
+          .expect(200);
+    });
   });
 });
